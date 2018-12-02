ac200753599c5fc919359ccf0608fef85876649a
Kezun Sima <simakezun@outlook.com>
2018-12-01 18:04:46 -0800

    update

--- ubuntu/libs.sh
+++ ubuntu/libs.sh
@@ -28,6 +28,7 @@ function git_clone() {
     )
 }
 
+echo "try "rm /usr/local/lib/libfolly.so" if there are undefined folly functions"
 
 if [ "$run" = "apt" ]; then run=""; fi
 if [ "$run" = "" ]; then
@@ -101,7 +102,7 @@ fi
 
 if [ "$run" = "mstch" ]; then run=""; fi
 if [ "$run" = "" ]; then
-    git_clone https://github.com/no1msd/mstch
+    git_clone https://github.com/no1msd/mstch.git
     cd mstch
     mkdir build
     cd build
@@ -149,20 +150,28 @@ fi
 
 if [ "$run" = "folly" ]; then run=""; fi
 if [ "$run" = "" ]; then
-    git_clone https://github.com/facebook/folly
+    git_clone https://github.com/facebook/folly.git
     cd folly
+
+    # build and install libfolly.so
+    rm -fr CMakeCache.txt
+    cmake configure . -DBUILD_SHARED_LIBS=ON
+    make -j $(nproc)
+    sudo make install
     
     # add -fPIC to CMake/FollyCompilerUnix.cmake
-    sed -i 's/-fsigned-char/-fsigned-char -fsigned-char/;' CMake/FollyCompilerUnix.cmake
-    cmake . && \
-      make -j $(nproc) && \
-      sudo make install
+    sed -i 's/-fsigned-char/-fsigned-char -fPIC/;' CMake/FollyCompilerUnix.cmake
+    rm -fr CMakeCache.txt
+    cmake .
+    make -j $(nproc)
+    sudo make install
+
     cd ..
 fi
 
 if [ "$run" = "fizz" ]; then run=""; fi
 if [ "$run" = "" ]; then
-    git_clone https://github.com/facebookincubator/fizz
+    git_clone https://github.com/facebookincubator/fizz.git
     mkdir fizz/build_ && cd fizz/build_
     add_glog_cmake_dep ../fizz
     cmake ../fizz
@@ -232,4 +241,15 @@ if [ "$run" = "" ]; then
     make
     sudo make install
     cd ../..
+
+    # If you ever happen to want to link against installed libraries
+    # in a given directory, LIBDIR, you must either use libtool, and
+    # specify the full pathname of the library, or use the '-LLIBDIR'
+    # flag during linking and do at least one of the following:
+    #    - add LIBDIR to the 'LD_LIBRARY_PATH' environment variable
+    #      during execution
+    #    - add LIBDIR to the 'LD_RUN_PATH' environment variable
+    #      during linking
+    #    - use the '-Wl,-rpath -Wl,LIBDIR' linker flag
+    #    - have your system administrator add LIBDIR to '/etc/ld.so.conf'
 fi

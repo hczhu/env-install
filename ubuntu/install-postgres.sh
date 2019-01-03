4077d58d305f68cb4e54f4c134f92825a06bf1ee
Kezun Sima <simakezun@outlook.com>
2019-01-03 04:15:56 +0000

    update

--- ubuntu/install-postgres.sh
+++ ubuntu/install-postgres.sh
@@ -11,3 +11,12 @@ cd postgres
 ./configure
 make
 sudo make install
+make clean
+sudo adduser postgres
+sudo mkdir /usr/local/pgsql/data
+su - postgres
+
+/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data
+/usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data >logfile 2>&1 &
+/usr/local/pgsql/bin/createdb test
+/usr/local/pgsql/bin/psql test

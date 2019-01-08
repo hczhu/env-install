0bc2e4b456f838596318857a96a38785d2e1b6ce
Kezun Sima <simakezun@outlook.com>
2019-01-07 16:13:26 -0800

    update

--- ubuntu/common-packages.sh
+++ ubuntu/common-packages.sh
@@ -32,12 +32,15 @@ sudo apt-get install     libiberty-dev
 # sudo apt-get install Mercurial
 # sudo apt-get upgrade pip3
 # sudo apt-get upgrade pip3.4
-sudo apt-get install gcc
-sudo apt-get upgrade gcc
+
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install gcc-8 g++-8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 10
+
+
 sudo apt-get install openjdk-8-jdk
 # sudo add-apt-repository ppa:webupd8team/java
 # sudo apt-get update && sudo apt-get install oracle-java8-installer

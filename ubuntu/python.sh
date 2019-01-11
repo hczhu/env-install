0e8be051b04cbf3445cf402773e2aa65f7d503cf
Kezun Sima <simakezun@outlook.com>
2019-01-10 21:38:30 -0800

    u

--- ubuntu/python.sh
+++ ubuntu/python.sh
@@ -17,6 +17,7 @@ sudo apt-get install -y python3-dev
 sudo apt-get install -y python3-setuptools
 sudo apt-get install -y wget
 sudo apt install python3-dev libpython3-dev
+sudo apt-get install libmysqlclient-dev
 
 # Prepare to build
 mkdir /tmp/Python37
@@ -32,4 +33,6 @@ update-alternatives --list python
 sudo update-alternatives --install /usr/bin/python python /usr/local/bin/python3.7 1
 update-alternatives --list python
 
-sudo pip3 install future six
+sudo pip3.7 install future six python-dateutil mysqlclient mmh3
+
+sudo pip3.7 install black
